2e98906bee4afe5688a94099a7f4402304e6624e
IPKIN9 <irwandirini@gmail.com>
2022-09-20 21:00:53 +0800

    Install: CMS template

---
+++ resources/views/Pages/Dashboard.blade.php
@@ -0,0 +1,13 @@
+@extends('Base.Skelton')
+@section('head-content')
+    <div class="col-md-12">
+        <div class="page-header-title">
+            <h5 class="m-b-10">Dashboard Analytics</h5>
+        </div>
+        <ul class="breadcrumb">
+            <li class="breadcrumb-item"><a href="index.html"><i class="feather icon-home"></i></a>
+            </li>
+            <li class="breadcrumb-item"><a href="#!">Dashboard Analytics</a></li>
+        </ul>
+    </div>
+@endsection
