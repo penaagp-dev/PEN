8e087798c22842033bd4f3beeae6ce0302cf963f
IPKIN9 <irwandirini@gmail.com>
2022-09-27 11:31:33 +0800

    Modify: view CRUD interaction

--- resources/views/Base/Skelton.blade.php
+++ resources/views/Base/Skelton.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <title>Ablepro v8.0 bootstrap admin template by Phoenixcoded</title>
-
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -19,6 +19,7 @@
         integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.4.34/dist/sweetalert2.all.js" integrity="sha256-dM3nV3SuUwK04X1Jp1Pul6Uqob1z+3igyB10aYS48xM=" crossorigin="anonymous"></script>
 
 
 
@@ -56,6 +57,31 @@
             </div>
             <!-- [ Main Content ] end -->
         </div>
+        <div class="modal fade" id="modalUpdate">
+            <div class="modal-dialog">
+                <div class="modal-content">
+            
+                <!-- Modal Header -->
+                <div class="modal-header">
+                    <h4 class="modal-title">Upsert Data</h4>
+                </div>
+            
+                <!-- Modal body -->
+                <div class="modal-body">
+                    <form id="form-upsert">
+                        @yield('form')
+                    </form>
+                </div>
+            
+                <!-- Modal footer -->
+                <div class="modal-footer" id="md-footer">
+                    <button type="button" class="btn btn-primary rounded" id="btn-send">Kirim</button>
+                    <button type="button" class="btn btn-danger mr-2 rounded" data-dismiss="modal">Tutup</button>
+                </div>
+            
+                </div>
+            </div>
+        </div>
     </div>
     <script src="assets/js/vendor-all.min.js"></script>
     <script src="assets/js/plugins/bootstrap.min.js"></script>
@@ -64,6 +90,7 @@
     <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap4.min.js"></script>
     <script src="assets/js/moment.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.4.34/dist/sweetalert2.all.min.js" integrity="sha256-VY+q0sN0i/R2qdIEtHeJ62U1Q1x1H7qvd08vkIJLDbc=" crossorigin="anonymous"></script>
     @yield('js-content')
 
 </body>
--- resources/views/Pages/Example.blade.php
+++ resources/views/Pages/Example.blade.php
@@ -18,65 +18,200 @@
 @section('main-content')
     <div class="col-xl-12">
         <div class="card">
+            <div class="card-header">
+              <button class="btn btn-primary rounded" id="btn-add">Tambah Data</button>
+            </div>
             <div class="card-body">
-                <ul class="nav nav-tabs mb-3 row" id="myTab" role="tablist">
-                    <li class="nav-item col-sm-2">
-                        <a class="nav-link active text-uppercase" id="table-tab" data-toggle="tab" href="#table"
-                            role="tab" aria-controls="table" aria-selected="true">Tabel</a>
-                    </li>
-                    <li class="nav-item col-sm-2">
-                        <a class="nav-link text-uppercase" id="form-tab" data-toggle="tab" href="#form" role="tab"
-                            aria-controls="form" aria-selected="false">Form</a>
-                    </li>
-                </ul>
-                <div class="tab-content" id="myTabContent">
-                    <div class="tab-pane fade show active" id="table" role="tabpanel" aria-labelledby="table-tab">
-                        <div class="table-responsive mt-4">
-                            <table class="table table-striped" id="table-example">
-                                <thead>
-                                    <tr>
-                                        <th style="width: 30px;">#</th>
-                                        <th>Sample</th>
-                                        <th>Is Text</th>
-                                        <th>Date Sample</th>
-                                        <th>Created</th>
-                                        <th style="width: 100px;">Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody id="tb-content">
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                    <div class="tab-pane fade" id="form" role="tabpanel" aria-labelledby="form-tab">
-                        <p class="mb-0">Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee
-                            squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan
-                            four
-                            loko
-                            farm-to-table
-                            craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts
-                            ullamco ad vinyl cillum PBR. accusamus tattooed echo park.</p>
-                    </div>
-                </div>
+              <div class="table-responsive mt-4">
+                <table class="table table-striped" id="table-example">
+                    <thead>
+                        <tr>
+                            <th style="width: 5%">#</th>
+                            <th>Sample</th>
+                            <th>Is Text</th>
+                            <th style="width: 10%">Date Sample</th>
+                            <th style="width: 12%">Created</th>
+                            <th style="width: 12%">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody id="tb-content">
+                    </tbody>
+                </table>
+              </div>
             </div>
         </div>
     </div>
 @endsection
+@section('form')
+<div class="row py-4">
+  <div class="col-md-6">
+    <div class="form-group fill">
+        <label >Sample</label>
+        <input type="hidden" name="id" id="id">
+        <input id="sample" name="sample" type="text" class="form-control" placeholder="input here..." autocomplete="off">
+        <small id="sample-alert" class="form-text text-danger"></small>
+    </div>
+  </div>
+  <div class="col-md-6">
+    <div class="form-group fill">
+        <label >Date Sample</label>
+        <input id="date_sample" name="date_sample" type="date" class="form-control">
+        <small id="date_sample-alert" class="form-text text-danger"></small>
+    </div>
+  </div>
+  <div class="col-md-12 mt-3">
+    <div class="form-group fill">
+        <label>Is Text</label>
+        <textarea id="is_text" name="is_text" class="form-control" rows="3"></textarea>
+        <small id="is_text-alert" class="form-text text-danger"></small>
+    </div>
+  </div>
+</div>
+@endsection
 @section('js-content')
     <script>
       let url = `{{ config('app.url') }}/v1/example`
+      const successAllert = () => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Good Job',
+          text: 'Data has been saved!'
+        }).then((res) => {
+          if (res.isConfirmed) {
+            window.location.reload()
+          }
+        })
+      }
+
+      const dangerAlert = () => {
+        Swal.fire({
+          icon: 'warning',
+          title: 'Error!',
+          text: 'Server sedang bermasalah'
+        }).then((res) => {
+          if (res.isConfirmed) {
+            window.location.reload()
+          }
+        })
+      }
 
       $(document).ready(() => {
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
         $.get(url, (res) => {
-          const table = $('#table-example').DataTable()
+          const table = $('#table-example').DataTable({
+            "bAutoWidth": false
+          })
           $.each(res.data, (i, val) => {
             table.row.add([
-              i+1, val.sample, val.is_text, val.date_sample, moment(val.created_at).format("DD MMMM YYYY"),
-              `<button class="btn btn-sm btn-outline-primary rounded"><i class="fa-regular fa-pen-to-square"></i></button>
-              <button class="btn btn-sm btn-outline-secondary rounded ml-1"><i class="fa-regular fa-trash-can"></i></button>`
+              i+1 + '.', val.sample, val.is_text, val.date_sample, moment(val.created_at).format("DD MMMM YYYY"),
+              `<button class="btn btn-sm btn-outline-primary rounded" id="btn-edit" data-id="${val.id}"><i class="fa-regular fa-pen-to-square"></i></button>
+              <button class="btn btn-sm btn-outline-secondary rounded ml-1" id="btn-del" data-id="${val.id}"><i class="fa-regular fa-trash-can"></i></button>`
             ])
             .draw()
           })
+        })
+      })
+
+      const proccessBtn = () => {
+        $('#btn-send').prop('disabled', true)
+        $('#btn-send').html(`Loading
+          <div class="spinner-border spinner-border-sm ml-2" role="status">
+            <span class="sr-only">Loading...</span>
+          </div>
+        `)
+      }
+
+      const disableSpinner = () => {
+        $('#btn-send').prop('disabled', false)
+        $('#btn-send').html('Kirim')
+      }
+
+      const fieldList = ['id', 'sample', 'date_sample', 'is_text']
+
+      const clear = () => {
+        $.each(fieldList, (i, val) => {
+          $(`#${val}`).val('')
+        })
+      }
+
+      $(document).on('click', '#btn-add', function() {
+        clear()
+        $('#modalUpdate').modal('show')
+      })
+
+      $(document).on('click', '#btn-send', () => {
+        let dataSample = $('#form-upsert').serialize()
+        proccessBtn()
+        $.ajax({
+          type: "POST",
+          url: url,
+          data: dataSample,
+          success: (result) => {
+            $('#modalUpdate').modal('hide')
+            successAllert()
+          },
+          error: (err) => {
+            let myErr = err.responseJSON
+            if (myErr.errors.length > 0) {
+              $.each(myErr.errors.data, (i, value) => {
+                $(`#${i}-alert`).html(value)
+              })
+              disableSpinner()
+            } else {
+              dangerAlert()
+            }
+          }
+        })
+      })
+
+      $(document).on('click', '#btn-edit', function() {
+        let _id = $(this).data('id')
+
+        $.get(url + '/' + _id, (result) => {
+          $.each(fieldList, (i, value) => {
+            $(`#${value}`).val(result.data[value])
+          })
+        })
+        $('#modalUpdate').modal('show')
+      })
+
+      $(document).on('click', '#btn-del', function() {
+        let _id = $(this).data('id')
+        Swal.fire({
+          title: 'Apakah anda yakin?',
+          text: "Data akan di hapus permanen!",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Ya, Hapus itu!',
+          cancelButtonText: 'Batal',
+        }).then((result) => {
+          if (result.isConfirmed) {
+            $.ajax({
+              type: "DELETE",
+              url: url + '/' + _id,
+              success: (result) => {
+                Swal.fire(
+                  'Deleted!',
+                  'Data berhasil di hapus.',
+                  'success'
+                ).then((result) => {
+                  if (result.isConfirmed) {
+                    window.location.reload()
+                  }
+                })
+              },
+              error: (err) => {
+                dangerAlert()
+              }
+            })
+          }
         })
       })
     </script>
