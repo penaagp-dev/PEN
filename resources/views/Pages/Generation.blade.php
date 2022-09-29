d0118a230961ffb8dab7a1c3926cf7961bb73ab2
Suryawandivta <gitdivta@gmail.com>
2022-09-29 21:51:25 +0800

    modifikasi fungsi create table Generation

--- resources/views/Pages/Generation.blade.php
+++ resources/views/Pages/Generation.blade.php
@@ -22,45 +22,22 @@
               <button class="btn btn-primary rounded" id="btn-add">Tambah Data</button>
             </div>
             <div class="card-body">
-                <ul class="nav nav-tabs mb-3 row" id="myTab" role="tablist">
-                    <li class="nav-item col-sm-2">
-                        <a class="nav-link active text-uppercase" id="table-tab" data-toggle="tab" href="#table"
-                            role="tab" aria-controls="table" aria-selected="true">Tabel</a>
-                    </li>
-                    <li class="nav-item col-sm-2">
-                        <a class="nav-link text-uppercase" id="form-tab" data-toggle="tab" href="#form" role="tab"
-                            aria-controls="form" aria-selected="false">Form</a>
-                    </li>
-                </ul>
-                <div class="tab-content" id="myTabContent">
-                    <div class="tab-pane fade show active" id="table" role="tabpanel" aria-labelledby="table-tab">
-                        <div class="table-responsive mt-4">
-                            <table class="table table-striped" id="table-generation">
-                                <thead>
-                                    <tr>
-                                        <th style="width: 30px;">#</th>
-                                        <th>Name</th>
-                                        <th>Years</th>
-                                        <th>Graduated</th>
-                                        <th>Created</th>
-                                        <th style="width: 100px;">Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody id="tb-content">
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                    <div class="tab-pane fade" id="form" role="tabpanel" aria-labelledby="form-tab">
-                        <p class="mb-0">Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee
-                            squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan
-                            four
-                            loko
-                            farm-to-table
-                            craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts
-                            ullamco ad vinyl cillum PBR. accusamus tattooed echo park.</p>
-                    </div>
-                </div>
+              <div class="table-responsive mt-4">
+                <table class="table table-striped" id="table-generation">
+                    <thead>
+                        <tr>
+                            <th style="width: 5%">#</th>
+                            <th>Name</th>
+                            <th>Years</th>
+                            <th >Graduated</th>
+                            <th ">Created</th>
+                            <th style="width: 12%">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody id="tb-content">
+                    </tbody>
+                </table>
+              </div>
             </div>
         </div>
     </div>
@@ -78,14 +55,14 @@
   <div class="col-md-6">
     <div class="form-group fill">
         <label >Years</label>
-        <input id="years" name="years" type="text" class="form-control">
+        <input id="years" name="years" type="text" class="form-control" placeholder="input here...">
         <small id="years-alert" class="form-text text-danger"></small>
     </div>
   </div>
   <div class="col-md-12 mt-3">
     <div class="form-group fill">
         <label>Graduated</label>
-        <input id="graduated" name="graduated" type="date" class="form-control">
+        <input id="graduated" type="date" name="graduated" class="form-control" ></input>
         <small id="graduated-alert" class="form-text text-danger"></small>
     </div>
   </div>
@@ -93,43 +70,15 @@
 @endsection
 @section('js-content')
     <script>
-    let url = `{{ config('app.url') }}/v1/generation`
-      const successAllert = () => {
-        Swal.fire({
-          icon: 'success',
-          title: 'Good Job',
-          text: 'Data has been saved!'
-        }).then((res) => {
-          if (res.isConfirmed) {
-            window.location.reload()
-          }
-        })
-      }
+      let url = `{{ config('app.url') }}/v1/generation`
 
-      const dangerAlert = () => {
-        Swal.fire({
-          icon: 'warning',
-          title: 'Error!',
-          text: 'Server sedang bermasalah'
-        }).then((res) => {
-          if (res.isConfirmed) {
-            window.location.reload()
-          }
-        })
-      }
-     
-
-      $(document).ready(() => {
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-
-        $.get(url, (res) => {
-          const table = $('#table-generation').DataTable({
+      const table = $('#table-generation').DataTable({
             "bAutoWidth": false
-          })
+      })
+
+      const getGeneration = () => {
+        table.clear()
+        $.get(url, (res) => {
           $.each(res.data, (i, val) => {
             table.row.add([
               i+1 + '.', val.name, val.years, val.graduated, moment(val.created_at).format("DD MMMM YYYY"),
@@ -139,8 +88,39 @@
             .draw()
           })
         })
+      }
+
+      $(document).ready(() => {
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+        getGeneration()
       })
-       const proccessBtn = () => {
+
+      const successAllert = () => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Good Job',
+          text: 'Data has been saved!'
+        }).then((res) => {
+          if (res.isConfirmed) {
+            getGeneration()
+            clear()
+          }
+        })
+      }
+
+      const dangerAlert = () => {
+        Swal.fire({
+          icon: 'warning',
+          title: 'Error!',
+          text: 'Server sedang bermasalah'
+        })
+      }
+
+      const proccessBtn = () => {
         $('#btn-send').prop('disabled', true)
         $('#btn-send').html(`Loading
           <div class="spinner-border spinner-border-sm ml-2" role="status">
@@ -159,6 +139,7 @@
       const clear = () => {
         $.each(fieldList, (i, val) => {
           $(`#${val}`).val('')
+          disableSpinner()
         })
       }
 
@@ -192,5 +173,6 @@
         })
       })
 
+     
     </script>
 @endsection
